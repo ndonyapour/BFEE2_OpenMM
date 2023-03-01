e669b1b1c8eb8433b1da179fcc78eacccc21d36f
Nazanin Donyapour <nazanin.donyapour@gmail.com>
2023-03-01 02:56:09 +0000

    fix polartheta and polarphi CVs issue

--- openmm_plumed/000_eq/report_timing.py
+++ openmm_plumed/000_eq/report_timing.py
@@ -18,6 +18,27 @@ import parmed as pmd
 import time
 
 
+def report_timing(system, positions, description):
+    """Report timing on all available platforms."""
+    timestep = 2.0*unit.femtoseconds
+    nsteps = 5000
+    for platform_name in ['CUDA']:
+        platform = omm.Platform.getPlatformByName(platform_name)
+
+        integrator = omm.LangevinIntegrator(300*unit.kelvin, 1.0/unit.picoseconds, timestep)
+        context = omm.Context(system, integrator, platform)
+        context.setPositions(positions)
+        # Warm up the integrator
+        integrator.step(10)
+        # Time integration
+        initial_time = time.time()
+        integrator.step(nsteps)
+        final_time = time.time()
+        elapsed_time = (final_time - initial_time) * unit.seconds
+        ns_per_day = nsteps * timestep / elapsed_time / (unit.nanoseconds / unit.day)
+        print('%64s : %16s : %8.3f ns/day\n' % (description, platform_name, ns_per_day))
+        del context, integrator
+
 # from wepy, to restart a simulation
 GET_STATE_KWARG_DEFAULTS = (('getPositions', True),
                             ('getVelocities', True),
@@ -82,57 +103,4 @@ with open(plumed_file, 'r') as file:
 system.addForce(PlumedForce(script))
 
 # make the integrator
-integrator = omm.LangevinIntegrator(TEMPERATURE, FRICTION_COEFFICIENT, STEP_SIZE)
-
-platform = omm.Platform.getPlatformByName(PLATFORM)
-prop = dict(Precision=PRECISION)
-
-simulation = omma.Simulation(prmtop.topology, system, integrator, platform, prop)
-
-print("New Simulation")
-simulation.context.setPositions(coords)
-
-simulation.reporters.append(mdj.reporters.DCDReporter(osp.join(OUTPUTS_PATH, SIM_TRAJ),
-                                                                DCD_REPORT_STEPS))
-                                                                #atomSubset=protein_ligand_idxs))
-
-simulation.reporters.append(omma.CheckpointReporter(checkpoint_path,
-                                                    CHECKPOINT_REPORTER_STEPS))
-
-simulation.reporters.append(
-    omma.StateDataReporter(
-        "log",
-        LOG_REPORTER_STEPS,
-        step=True,
-        time=True,
-        potentialEnergy=True,
-        kineticEnergy=True,
-        totalEnergy=True,
-        volume=True,
-        temperature=True,
-        totalSteps=True,
-        separator=" ",
-    )
-)
-
-print("Start Simulation")
-start_time = time.time()
-simulation.step(NUM_STEPS)
-end_time = time.time()
-print("End Simulation")
-print(f"Run time = {np.round(end_time - start_time, 3)}s")
-simulation_time = round((STEP_SIZE * NUM_STEPS).value_in_unit(unit.nanoseconds),
-                           2)
-print(f"Simulation time: {simulation_time}ns")
-simulation.saveCheckpoint(osp.join(OUTPUTS_PATH, CHECKPOINT_LAST))
-
-# save final state and system
-get_state_kwargs = dict(GET_STATE_KWARG_DEFAULTS)
-omm_state = simulation.context.getState(**get_state_kwargs)
-# save the pkl files to the inputs dir
-with open(osp.join(OUTPUTS_PATH, SYSTEM_FILE), 'wb') as wfile:
-    pkl.dump(system, wfile)
-
-with open(osp.join(OUTPUTS_PATH, OMM_STATE_FILE), 'wb') as wfile:
-    pkl.dump(omm_state, wfile)
-print('Done making pkls. Check inputs dir for them!')
+report_timing(system, coords, "Eq step:")
