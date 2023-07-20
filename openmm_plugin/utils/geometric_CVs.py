b2c8aac98707926abbdba4a8e581a7ee77f6340b
Nazanin Donyapour <nazanin.donyapour@gmail.com>
2023-07-20 21:19:14 +0000

    add polar, r, translational, and rmsd CVs

--- openmm_plugin/utils/geometric_CVs.py
+++ openmm_plugin/utils/geometric_CVs.py
@@ -2,6 +2,8 @@ import os
 import numpy as np
 import math
 
+import mdtraj as mdj
+
 def calculateCOG(pos):
     return np.mean(pos, axis=0)
 
@@ -31,11 +33,6 @@ class GeometricCVs:
             return rot_q
 
         else:
-            print("I'm here")
-            # second group used for fitting 
-            # center ref pos
-            # import ipdb 
-            # ipdb.set_trace()
             centered_refpos = shiftbyCOG(refpositions)
             centered_refpos_fitgroup = shiftbyCOG(fitting_refpositions)
 
@@ -58,26 +55,102 @@ class GeometricCVs:
 
     def EuelrAngle(self, refpositions, positions, group_idxs, fittingGroup_idxs=None, angle='Theta'):
         angles = []
-        qs = []
+        #qs = []
         enable_fitting = False if fittingGroup_idxs is None else True
         radian_to_degree = 180 / 3.1415926
         for idx in range(positions.shape[0]):
             rot_q = self.calc_quaternion(refpositions[group_idxs], positions[idx][group_idxs], 
                                          refpositions[fittingGroup_idxs], positions[idx][fittingGroup_idxs], 
                                          enable_fitting)
-            # if idx == 114:
-            #     import ipdb
-            #     ipdb.set_trace()
-            #if (angle == "Theta") {
-            qs.append(rot_q)
+   
             q1 = rot_q[0]  
             q2 = rot_q[1] 
             q3 = rot_q[2] 
             q4 = rot_q[3]
-            x = 2 * (q1 * q3 - q4 * q2)
-            angles.append(radian_to_degree * math.asin(x))
+            
+            if angle=='Theta':
+                x = 2 * (q1 * q3 - q4 * q2)
+                angles.append(radian_to_degree * math.asin(x))
+            elif angle=='Phi':
+                x = 2*(q1*q2+q3*q4)
+                y = 1-2*(q2*q2+q3*q3)
+                angles.append(radian_to_degree * math.atan2(x, y))
+            elif angle=='Psi':
+                x = 2*(q1*q4+q2*q3)
+                y = 1-2*(q3*q3+q4*q4)
+                angles.append(radian_to_degree * math.atan2(x, y))
+            elif angle=='all':
+                x = 2 * (q1 * q3 - q4 * q2)
+                theta = radian_to_degree * math.asin(x)
+                x = 2*(q1*q2+q3*q4)
+                y = 1-2*(q2*q2+q3*q3)
+                phi= radian_to_degree * math.atan2(x, y)
+                x = 2*(q1*q4+q2*q3)
+                y = 1-2*(q3*q3+q4*q4)
+                psi = radian_to_degree * math.atan2(x, y)
+                angles.append([theta, phi, psi])
+            else:
+                print("The angle type is wrong")
+                exit()
         return np.array(angles)
+    
+    def PolarAngle(self, refpositions, positions, group_idxs, fittingGroup_idxs, angle='Theta'):
+        angles = []
+        #qs = []
+        enable_fitting = False if fittingGroup_idxs is None else True
+        radian_to_degree = 180 / 3.1415926
+        for idx in range(positions.shape[0]):
+            # centered_refpos = shiftbyCOG(refpositions[group_idxs])
+            centered_refpos_fitgroup = shiftbyCOG(refpositions[fittingGroup_idxs])
 
+            # calc COG of the fitting group
+            fitgroup_COG = calculateCOG(positions[idx][fittingGroup_idxs])
+            centered_pos = translateCoordinates(positions[idx][group_idxs], fitgroup_COG)
+            centered_pos_fitgroup = translateCoordinates(positions[idx][fittingGroup_idxs], fitgroup_COG)
+            quaternion = Quaternion()
+            fit_rot_q = quaternion.calc_optimal_rotation(centered_pos_fitgroup, centered_refpos_fitgroup)
+            rotated_pos = quaternion.rotateCoordinates(fit_rot_q, centered_pos)
+            rotated_fitpos = quaternion.rotateCoordinates(fit_rot_q, centered_pos_fitgroup)
+   
+                
+            fit_rot_pos_cog = calculateCOG(rotated_fitpos)
+            rot_pos_cog = calculateCOG(rotated_pos)
+            distance = rot_pos_cog - fit_rot_pos_cog
+            norm = np.sqrt(np.dot(distance, distance))
+            unit_vec = distance / norm
+            i1 = unit_vec[0]
+            i2 = unit_vec[1]
+            i3 = unit_vec[2]
+            if angle == "Theta":
+                angles.append(radian_to_degree * math.acos(-i2))
+
+            elif angle=='Phi':
+                angles.append(radian_to_degree * math.atan2(i3, i1))
+   
+            elif angle=='all':
+                theta = radian_to_degree * math.acos(-i2)
+                phi = radian_to_degree * math.atan2(i3, i1)
+                angles.append([theta, phi])
+            else:
+                print("The angle type is wrong")
+                exit()
+        return np.array(angles)
+    
+    def rmsd(self, pdb, traj, atom_idxs):
+
+        return mdj.rmsd(pdb, traj, atom_indices=atom_idxs)
+    
+    def r(self, traj, selection1, seletion2):
+        coms1 = mdj.compute_center_of_mass(traj, select=selection1)
+        coms2 = mdj.compute_center_of_mass(traj, select=seletion2)
+        return np.linalg.norm(coms1 - coms2, axis=1)
+    
+    def translation(sef, ref_pdb, traj, selection):
+        ref_com = mdj.compute_center_of_mass(ref_pdb, select=selection)
+        coms = mdj.compute_center_of_mass(traj, select=selection)
+        return np.linalg.norm(coms - ref_com, axis=1)
+
+# np.max(translation_cv)
 
 
 class Quaternion:
@@ -152,19 +225,18 @@ class Quaternion:
         # U, S, V = np.linalg.svd(self.S)
         # self.S_eigval = S**2 / (S**2).sum()
         # self.S_eigvec = V
-        from scipy.linalg import eig
         self.S_eigval, self.S_eigvec = np.linalg.eig(S)
-        print(self.S_eigvec)
         # print("************************")
 
         # convert complex values to real
-        # self.S_eigval = np.real(self.S_eigval)
-        # self.S_eigvec = np.real(self.S_eigvec)
+        self.S_eigval = np.real(self.S_eigval)
+        self.S_eigvec = np.real(self.S_eigvec)
 
     def getQfromEigenvecs(self, idx):
         # Note the first column is not the first eigen vector
         eig_idx = np.argmax(np.abs(self.S_eigvec[0, :]))
         self.Swap(self.S_eigvec, 0, eig_idx)
+        self.S_eigval[0], self.S_eigval[eig_idx] =  self.S_eigval[eig_idx], self.S_eigval[0]
         normquat = np.array(self.normquat)
         if np.matmul(self.S_eigvec[:, idx], normquat) < 0:
             return -1 * self.S_eigvec[:, idx]
@@ -192,4 +264,6 @@ class Quaternion:
         # a = np.cross(vec, vq) + q0 * vec
         # b = np.cross(a, vq)
         return b + b + vec
+    def getEigenValue(self, idx):
+        return self.S_eigval[idx]
 
